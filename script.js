d4ab39dfe2fce1b42cddee85756710d61b7ef298
AnkY <ank0409@gmail.com>
2020-07-24 21:59:55 +0530

    published

---
+++ script.js
@@ -0,0 +1,42 @@
+var btn1_click = document.querySelector(".btn1")
+
+btn1_click.addEventListener('click', function() {
+    var role1 = document.querySelector(".role1")
+    var role_summ = document.querySelector(".role-summary")
+    if (role1.style.display === "none") {
+        role1.style.display = "block";
+        role_summ.style.display = "none";
+    } else {
+        role1.style.display = "none";
+        role_summ.style.display = "block";
+    }
+})
+
+var btn1_click = document.querySelector(".btn2")
+
+btn1_click.addEventListener('click', function() {
+    
+    var role1 = document.querySelector(".role2")
+    var role_summ = document.querySelector(".role-summary1")
+    if (role1.style.display === "none") {
+        role1.style.display = "block";
+        role_summ.style.display = "none";
+    } else {
+        role1.style.display = "none";
+        role_summ.style.display = "block";
+    }
+})
+
+var btn1_click = document.querySelector(".btn3")
+
+btn1_click.addEventListener('click', function() {
+    var role1 = document.querySelector(".role3")
+    var role_summ = document.querySelector(".role-summary2")
+    if (role1.style.display === "none") {
+        role1.style.display = "block";
+        role_summ.style.display = "none";
+    } else {
+        role1.style.display = "none";
+        role_summ.style.display = "block";
+    }
+})
